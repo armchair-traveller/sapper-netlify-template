36d3daa58d13fbee07d08c7622246a9d3d4b18e9
Armchair Traveller <sentient.sober.armchair@gmail.com>
2020-10-19 05:33:40 -0700

    clarify layout's purpose

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,4 +1,4 @@
-<!-- Layout is for any layout components like Nav. 
+<!-- Layout is for any layout components like Header/Nav, Footer, or a container. 
   Similar to layout in Gatsby, present on all pages -->
 <script>
 </script>
